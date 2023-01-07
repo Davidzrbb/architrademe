54a41813ae9326aaeca18a53698d411b3077278e
David <zdavid1@myges.fr>
2023-01-07 13:27:13 +0100

    start web with h2

---
+++ bootstrap/src/main/java/arch/hex/bootstrap/ArchiTradeMeApplication.java
@@ -0,0 +1,11 @@
+package arch.hex.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiTradeMeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ArchiTradeMeApplication.class, args);
+	}
+}
